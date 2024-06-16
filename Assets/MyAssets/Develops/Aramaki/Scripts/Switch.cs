dab14f1968d8a27f7adea8e1cebf992be7719665
Shuto.4416 <shuto.4416.git@gmail.com>
2024-06-16 11:36:33 +0900

    スイッチのバグを修正

--- Assets/MyAssets/Develops/Aramaki/Scripts/Switch.cs
+++ Assets/MyAssets/Develops/Aramaki/Scripts/Switch.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 public class Switch : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-<<<<<<< HEAD
     public bool button = false;
-=======
-    bool button;
->>>>>>> afba1359ac10af9534e3838e5a2054fb99759dfd
     void Start()
     {
         
@@ -19,19 +15,9 @@ public class Switch : MonoBehaviour
         
     }
 
-<<<<<<< HEAD
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.CompareTag("pusher")) button = true;
-=======
-    void FixedUpdate()
-    {
-        button = false;
-    }
 
     void OnCollisionStay2D(Collision2D other)
     {
         button = true;
->>>>>>> afba1359ac10af9534e3838e5a2054fb99759dfd
     }
 }
